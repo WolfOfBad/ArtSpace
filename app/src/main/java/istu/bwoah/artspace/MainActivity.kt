8bb94c8f77a705163c2259acc5345f8228a105af
bwoah <mechkov.igor04@gmail.com>
2025-03-27 13:36:59 +0400

    Rework surface and sizings

--- app/src/main/java/istu/bwoah/artspace/MainActivity.kt
+++ app/src/main/java/istu/bwoah/artspace/MainActivity.kt
@@ -1,5 +1,6 @@
 package istu.bwoah.artspace
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,13 +10,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -27,6 +29,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
@@ -41,64 +44,63 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val configuration = LocalConfiguration.current
-            val isTablet = configuration.screenWidthDp >= 600
-            val isPhone = configuration.screenHeightDp <= 600
 
             ArtSpaceTheme {
-                ArtSpaceApp(isTablet, isPhone)
+                ArtSpaceApp()
             }
         }
     }
 }
 
 @Composable
-fun ArtSpaceApp(isTablet: Boolean, isPhone: Boolean) {
+fun ArtSpaceApp() {
+    val configuration = LocalConfiguration.current
+    val isPortrait = configuration.orientation == Configuration.ORIENTATION_PORTRAIT
     var currentIndex by rememberSaveable { mutableIntStateOf(0) }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(if (isTablet && isPhone) 4.dp else 10.dp),
+            .padding(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom
+        verticalArrangement = Arrangement.Center
     ) {
-        ArtImage(isTablet, currentIndex)
+        ArtImage(isPortrait, currentIndex)
 
-        Spacer(modifier = Modifier.height(if (isTablet && isPhone) 5.dp else 40.dp))
+        Spacer(modifier = Modifier.height(15.dp))
 
-        ArtistInfo(isTablet, isPhone, currentIndex)
+        ArtistInfo(currentIndex)
 
-        Spacer(modifier = Modifier.height(if (isTablet or (isTablet && isPhone)) 5.dp else 20.dp))
+        Spacer(modifier = Modifier.height(if (isPortrait) 20.dp else 5.dp))
 
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(if (isTablet && isPhone) 5.dp else 20.dp),
+                .padding(if (isPortrait) 20.dp else 5.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Button(
                 modifier = Modifier.size(
-                    width = if (isTablet) 160.dp else 100.dp,
-                    height = if (isTablet) 60.dp else 40.dp
+                    width = if (!isPortrait) 160.dp else 100.dp,
+                    height = if (!isPortrait) 60.dp else 40.dp
                 ),
                 onClick = { currentIndex = (currentIndex + artworks.size - 1) % artworks.size }
             ) {
                 Text(
                     text = "Back",
-                    fontSize = if (isTablet) 20.sp else 16.sp
+                    fontSize = if (!isPortrait) 20.sp else 16.sp
                 )
             }
             Button(
                 modifier = Modifier.size(
-                    width = if (isTablet) 160.dp else 100.dp,
-                    height = if (isTablet) 60.dp else 40.dp
+                    width = if (!isPortrait) 160.dp else 100.dp,
+                    height = if (!isPortrait) 60.dp else 40.dp
                 ),
                 onClick = { currentIndex = ++currentIndex % artworks.size }
             ) {
                 Text(
                     text = "Next",
-                    fontSize = if (isTablet) 20.sp else 16.sp
+                    fontSize = if (!isPortrait) 20.sp else 16.sp
                 )
             }
         }
@@ -106,12 +108,14 @@ fun ArtSpaceApp(isTablet: Boolean, isPhone: Boolean) {
 }
 
 @Composable
-fun ArtImage(isTablet: Boolean, currentIndex: Int) {
+fun ArtImage(isPortrait: Boolean, currentIndex: Int) {
+    val maxHeight = if (isPortrait) 600.dp else 220.dp
+    val maxWidth = if (isPortrait) 350.dp else 400.dp
     Surface(
         modifier = Modifier
-            .fillMaxHeight(if (isTablet) 0.65f else 0.6f)
-            .aspectRatio(3f / 4f),
-        shadowElevation = 16.dp,
+            .wrapContentSize()
+            .sizeIn(maxHeight = maxHeight, maxWidth = maxWidth)
+            .shadow(16.dp, shape = RoundedCornerShape(8.dp)),
         color = Color.White
     ) {
         Image(
@@ -119,32 +123,32 @@ fun ArtImage(isTablet: Boolean, currentIndex: Int) {
             contentDescription = artworks[currentIndex].title,
             contentScale = ContentScale.Fit,
             modifier = Modifier
-                .fillMaxSize()
+                .wrapContentSize()
                 .padding(16.dp)
         )
     }
 }
 
 @Composable
-fun ArtistInfo(isTablet: Boolean, isPhone: Boolean, currentIndex: Int) {
+fun ArtistInfo(currentIndex: Int) {
     Column(
         Modifier
             .background(color = Color(0xFFdbdbdb))
-            .padding(if (isTablet && isPhone) 1.dp else 8.dp)
+            .padding(5.dp)
     )
     {
         Text(
             text = artworks[currentIndex].title,
             style = MaterialTheme.typography.headlineMedium.copy(
                 fontWeight = FontWeight.Bold,
-                fontSize = if (isTablet && isPhone) 16.sp else 24.sp
+                fontSize = 24.sp
             )
         )
-        Spacer(modifier = Modifier.size(if (isTablet && isPhone) 0.dp else 2.dp))
+        Spacer(modifier = Modifier.size(2.dp))
         Text(
             text = "${artworks[currentIndex].artist}, ${artworks[currentIndex].year}",
             style = MaterialTheme.typography.bodyMedium.copy(
-                fontSize = if (isTablet && isPhone) 12.sp else 20.sp
+                fontSize = 18.sp
             )
         )
     }
@@ -154,6 +158,6 @@ fun ArtistInfo(isTablet: Boolean, isPhone: Boolean, currentIndex: Int) {
 @Composable
 fun ArtSpacePreview() {
     ArtSpaceTheme {
-        ArtSpaceApp(isTablet = false, isPhone = true)
+        ArtSpaceApp()
     }
 }
